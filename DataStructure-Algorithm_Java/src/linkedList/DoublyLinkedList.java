230609b095026854fa3cd9d1eecb3831aa2ada1d
NickyViNi <happylemonlcm@gmail.com>
2024-04-07 23:58:41 -0700

    print dll

--- DataStructure-Algorithm_Java/src/linkedList/DoublyLinkedList.java
+++ DataStructure-Algorithm_Java/src/linkedList/DoublyLinkedList.java
@@ -21,4 +21,24 @@ public class DoublyLinkedList {
         this.tail = newNode;
         this.length = 1;
     }
+
+    public void printList() {
+        Node temp = this.head;
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public void getHead() {
+        System.out.println("Head: " + this.head.value);
+    }
+
+    public void getTail() {
+        System.out.println("Tail: " + this.tail.value);
+    }
+
+    public void getLength) {
+        System.out.println("Length: " + this.length);
+    }
 }
