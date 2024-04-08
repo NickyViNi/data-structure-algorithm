9269a6c1fde82f67f632853e8c6277105ae658d2
NickyViNi <happylemonlcm@gmail.com>
2024-04-08 14:01:39 -0700

    dll append

--- DataStructure-Algorithm_Java/src/linkedList/DoublyLinkedList.java
+++ DataStructure-Algorithm_Java/src/linkedList/DoublyLinkedList.java
@@ -22,12 +22,17 @@ public class DoublyLinkedList {
         this.length = 1;
     }
 
-    public void printList() {
-        Node temp = this.head;
-        while (temp != null) {
-            System.out.println(temp.value);
-            temp = temp.next;
+    public void append(int value) {
+        Node newNode = new Node(value);
+        if (this.length == 0) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            newNode.prev = this.tail;
+            this.tail = newNode;
         }
+        this.length++;
     }
 
     public void getHead() {
@@ -38,7 +43,32 @@ public class DoublyLinkedList {
         System.out.println("Tail: " + this.tail.value);
     }
 
-    public void getLength) {
+    public void getLength () {
         System.out.println("Length: " + this.length);
+    }
+
+    public void printList() {
+        Node temp = this.head;
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public void printAll() {
+        if (length == 0) {
+            System.out.println("Head: null");
+            System.out.println("Tail: null");
+        } else {
+            System.out.println("Head: " + head.value);
+            System.out.println("Tail: " + tail.value);
+        }
+        System.out.println("Length:" + length);
+        System.out.println("\nDoubly Linked List:");
+        if (length == 0) {
+            System.out.println("empty");
+        } else {
+            printList();
+        }
     }
 }
