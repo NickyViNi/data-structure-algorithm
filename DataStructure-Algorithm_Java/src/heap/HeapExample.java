8a02e7899c70c2f09a399aaae5e0a3e7cbef0d7e
NickyViNi <happylemonlcm@gmail.com>
2024-04-09 10:44:07 -0700

    priority heap methods

--- DataStructure-Algorithm_Java/src/heap/HeapExample.java
+++ DataStructure-Algorithm_Java/src/heap/HeapExample.java
@@ -4,6 +4,7 @@ import java.util.Comparator;
 
 public class HeapExample {
     public static void main(String[] args) {
+
         //(1) Max Heap
         PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Comparator.reverseOrder());
         maxHeap.add(3);
@@ -11,8 +12,15 @@ public class HeapExample {
         maxHeap.add(1);
         maxHeap.add(4);
         maxHeap.add(2);
+        maxHeap.offer(18);
+        maxHeap.offer(7);
 
         System.out.println("Max Heap:");
+        System.out.println(maxHeap.contains(41)); //Returns true if this queue contains the specified element.
+        System.out.println(maxHeap.peek()); //Retrieves, but does not remove, the head of this queue, or returns null if this queue is empty.
+        System.out.println(maxHeap.poll()); // Retrieves and removes the head of this queue, or returns null if this queue is empty.
+        System.out.println(maxHeap.remove(7)); //Removes a single instance of the specified element from this queue, if it is present.
+
         while (!maxHeap.isEmpty()) {
             System.out.print(maxHeap.poll() + " ");
         }
