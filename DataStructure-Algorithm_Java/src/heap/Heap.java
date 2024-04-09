46e2f2f444d39234c15b923ef0223beb8b207219
NickyViNi <happylemonlcm@gmail.com>
2024-04-08 23:34:46 -0700

    heap

---
+++ DataStructure-Algorithm_Java/src/heap/Heap.java
@@ -0,0 +1,15 @@
+package heap;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Heap {
+    private List<Integer> heap;
+
+    public Heap() {
+        this.heap = new ArrayList<>();
+    }
+
+    public List<Integer> getHeap() {
+        return new ArrayList<>(heap);
+    }
+}
