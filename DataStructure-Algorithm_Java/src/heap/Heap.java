e16e1b17a8a3ebbc0ede637ce7d5ecf5964c28de
NickyViNi <happylemonlcm@gmail.com>
2024-04-08 23:43:14 -0700

    heap swap

--- DataStructure-Algorithm_Java/src/heap/Heap.java
+++ DataStructure-Algorithm_Java/src/heap/Heap.java
@@ -12,4 +12,22 @@ public class Heap {
     public List<Integer> getHeap() {
         return new ArrayList<>(heap);
     }
+
+    private int leftChild(int index) {
+        return index * 2 + 1;
+    }
+
+    private int rightChild(int index) {
+        return index * 2 + 2;
+    }
+
+    private int parent(int index) {
+        return (index - 1) / 2;
+    }
+
+    private void swap(int index1, int index2) {
+        int temp = heap.get(index1);
+        heap.set(index1, heap.get(index2));
+        heap.set(index2, temp);
+    }
 }
