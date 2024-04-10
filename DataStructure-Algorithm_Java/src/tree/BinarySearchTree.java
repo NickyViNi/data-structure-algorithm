b1036fbac80780fece03185dc78a5262be835059
NickyViNi <happylemonlcm@gmail.com>
2024-04-10 09:45:57 -0700

    insert BST

--- DataStructure-Algorithm_Java/src/tree/BinarySearchTree.java
+++ DataStructure-Algorithm_Java/src/tree/BinarySearchTree.java
@@ -17,4 +17,34 @@ public class BinarySearchTree {
         public Node getRoot() {
             return root;
         }
+
+        public boolean insert(int value) {
+
+            Node newNode = new Node(value);
+
+            if (this.root == null) {
+                this.root = newNode;
+                return true;
+            }
+
+            Node temp = this.root;
+            while(true) {
+                if (newNode.value == temp.value) return false;
+                else if (newNode.value < temp.value) {
+                    if (temp.left == null) {
+                        temp.left = newNode;
+                        return true;
+                    } else {
+                        temp = temp.left;
+                    }
+                } else {
+                    if (temp.right == null) {
+                        temp.right = newNode;
+                        return true;
+                    } else {
+                        temp = temp.right;
+                    }
+                }
+            }
+        }
 }
