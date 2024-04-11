c42e328051def13652abe75d44ade85f623b48f2
NickyViNi <happylemonlcm@gmail.com>
2024-04-11 11:09:34 -0700

    graph

---
+++ DataStructure-Algorithm_Java/src/graph/Graph.java
@@ -0,0 +1,25 @@
+package graph;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Graph {
+    private HashMap<String, ArrayList<String>> adjList = new HashMap<>();
+
+    public HashMap<String, ArrayList<String>> getAdjList() {
+        return adjList;
+    }
+
+    public void printGraph() {
+        System.out.println(adjList);
+    }
+
+	//ADDVERTEX METHOD: O(1) //
+	public boolean addVertex(String vertex) {
+	    if (this.adjList.get(vertex) == null) {
+	        adjList.put(vertex, new ArrayList<String>());
+	        return true;
+	    }
+	    return false;
+	}
+}
