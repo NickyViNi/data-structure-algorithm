ac13a79d008d0b8e28e84e887165048afe41faad
NickyViNi <happylemonlcm@gmail.com>
2024-04-13 13:55:00 -0700

    inefficient fibonacci recurssion

--- DataStructure-Algorithm_Java/src/recursion/RecursionDemo.java
+++ DataStructure-Algorithm_Java/src/recursion/RecursionDemo.java
@@ -5,11 +5,24 @@ public class RecursionDemo {
     //base case : must have return statement to avoid stack overflow
     //recursive case
     public static void main(String[] argskk) {
-        System.out.println("5: " + factorial(6));
+        System.out.println(fibonacci(15) + " count: " + count);
+        // System.out.println("5: " + factorial(6));
     }
 
     public static int factorial(int n) {
         if (n == 1) return 1;
         return n * factorial(n - 1);
     }
+
+    static Integer[] memoization = new Integer[30];
+    static int count = 0;
+
+    // inefficient  time: O(2^n)
+    public static int fibonacci(int n) {
+        count++;
+        if (n == 0 || n == 1) return n;
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+
+
 }
