ec20df2d30f819c6e14e0740f29a57657d449bc0
NickyViNi <happylemonlcm@gmail.com>
2024-04-13 14:23:34 -0700

    fibonacci for loop

--- DataStructure-Algorithm_Java/src/recursion/RecursionDemo.java
+++ DataStructure-Algorithm_Java/src/recursion/RecursionDemo.java
@@ -5,7 +5,7 @@ public class RecursionDemo {
     //base case : must have return statement to avoid stack overflow
     //recursive case
     public static void main(String[] argskk) {
-        System.out.println(fibonacci(15) + " count: " + count);
+        System.out.println(fibonacci3(15) + " count: " + count);
         // System.out.println("5: " + factorial(6));
     }
 
@@ -24,11 +24,22 @@ public class RecursionDemo {
 
     static Integer[] memoization = new Integer[30];
     //efficient O(2n - 1) => time: O(n)
-    public static int fibonacci(int n) {
+    public static int fibonacci2(int n) {
         count++; // compare test => when n = 15, count = 29
         if(memoization[n] != null) return memoization[n];
         if(n == 0 || n == 1) return n;
-        return memoization[n] = fibonacci(n - 1) + fibonacci(n - 2);
+        return memoization[n] = fibonacci2(n - 1) + fibonacci2(n - 2);
         // return memoization[n];
+    }
+
+    //time: O(n - 1)
+    public static int fibonacci3(int n) {
+        int[] fib = new int[n + 1];
+        fib[0] = 0;
+        fib[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            fib[i] = fib[i - 1] + fib[i - 2];
+        }
+        return fib[n];
     }
 }
