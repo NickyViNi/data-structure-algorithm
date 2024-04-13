2b614cc247d11007c890422ff9b5e3db1e834fcc
NickyViNi <happylemonlcm@gmail.com>
2024-04-12 18:05:42 -0700

    insertion sort

--- DataStructure-Algorithm_Java/src/sort/Sort.java
+++ DataStructure-Algorithm_Java/src/sort/Sort.java
@@ -32,9 +32,22 @@ public class Sort {
         }
     }
 
+    // (3): insertion sort
+    public static void insertionSort(int[] arr) {
+	    for (int i = 1; i < arr.length; i++) {
+	        int j = i - 1;
+	        int temp = arr[i];
+	        while (j >= 0 && temp < arr[j]) {
+	            arr[j+1] = arr[j];
+	            arr[j] = temp;
+	            j--;
+	        }
+	    }
+	}
+
     public static void main(String[] args) {
-        int[] arr = {5, 21, 4, 1, 6, 3};
-        selectionSort(arr);
+        int[] arr = {5, 21, 41, 1, 6, 3};
+        insertionSort(arr);
         System.out.println(Arrays.toString(arr));
     }
 }
