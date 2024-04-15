b72ce044bb15aa30a1b6bc8bf490baba2fab66d9
NickyViNi <happylemonlcm@gmail.com>
2024-04-15 16:50:39 -0700

    has cycle in LL

--- DataStructure-Algorithm_Java/src/linkedList/LLleetcode.java
+++ DataStructure-Algorithm_Java/src/linkedList/LLleetcode.java
@@ -11,6 +11,7 @@ public class LLleetcode {
         ListNode(int val, ListNode next) { this.val = val; this.next = next; }
     }
 
+    //876
     public ListNode middleNode(ListNode head) {
         ListNode fast = head;
         ListNode low = head;
@@ -19,5 +20,18 @@ public class LLleetcode {
             fast = fast.next.next;
         }
         return low;
+    }
+    //141
+    public boolean hasCycle(ListNode head) {
+        ListNode low = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            low = low.next;
+            if (fast == low) {
+                return true;
+            }
+        }
+        return false;
     }
 }
