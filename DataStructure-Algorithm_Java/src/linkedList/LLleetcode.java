7f51a1a37d47163937630e2dda7529c2fa4edcda
NickyViNi <happylemonlcm@gmail.com>
2024-04-15 16:50:58 -0700

    find Kth node in LL

--- DataStructure-Algorithm_Java/src/linkedList/LLleetcode.java
+++ DataStructure-Algorithm_Java/src/linkedList/LLleetcode.java
@@ -34,4 +34,37 @@ public class LLleetcode {
         }
         return false;
     }
+
+    public ListNode findKthFromEnd1(ListNode head, int k) {
+	    int len = 0;
+	    ListNode curr = head;
+	    while (curr != null) {
+	        curr = curr.next;
+	        len++;
+	    }
+	    if (k > len) return null;
+
+	    int targetIdx = len - k;
+	    ListNode temp = head;
+	    while (targetIdx > 0) {
+	        temp = temp.next;
+	        targetIdx--;
+	    }
+	    return temp;
+	}
+    public ListNode findKthFromEnd(ListNode head, int k) {
+        if (head == null) return null;
+        ListNode fast = head;
+        ListNode low = head;
+        for (int i = 1; i < k; i++) {
+            fast = fast.next;
+            if (fast == null) return null;
+        }
+        while (fast.next != null) {
+            fast = fast.next;
+            low = low.next;
+        }
+
+        return low;
+    }
 }
