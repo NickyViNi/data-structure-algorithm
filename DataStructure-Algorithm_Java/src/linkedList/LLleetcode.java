9857f51554d7d3d790933ce6515825a277cf0aee
NickyViNi <happylemonlcm@gmail.com>
2024-04-15 17:35:52 -0700

    #19 remove kth node

--- DataStructure-Algorithm_Java/src/linkedList/LLleetcode.java
+++ DataStructure-Algorithm_Java/src/linkedList/LLleetcode.java
@@ -67,4 +67,28 @@ public class LLleetcode {
 
         return low;
     }
+
+    // 19
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        if (head.next == null) return null;
+
+        ListNode fast = head, slow = head, prev = null;
+        for (int i = 1; i < n; i++) {
+            fast = fast.next;
+            if (fast == null) return null;
+        }
+
+        while (fast.next != null) {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        if (prev != null) {
+            prev.next = slow.next;
+        } else {
+            head = head.next;
+        }
+        return head;
+    }
 }
