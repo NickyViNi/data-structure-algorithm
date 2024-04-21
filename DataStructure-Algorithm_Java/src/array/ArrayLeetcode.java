3440be57a8862f90dc7315bb04634064ca92177e
NickyViNi <happylemonlcm@gmail.com>
2024-04-21 01:25:36 -0700

    remove ele

--- DataStructure-Algorithm_Java/src/array/ArrayLeetcode.java
+++ DataStructure-Algorithm_Java/src/array/ArrayLeetcode.java
@@ -11,4 +11,22 @@ public class ArrayLeetcode {
         }
         return new int[] {max, min};
     }
+
+    //way1: remove all occurrences of val in-place from the array and return the new length of the array after removal.
+    public static int removeElement(int[] nums, int val) {
+        int count = 0;
+        for (int i = 0; i < nums.length - count; i++) {
+            if (nums[i] == val) {
+
+                for (int k = i; k < nums.length - count - 1; k++) {
+                    nums[k] = nums[k + 1];
+                }
+                count++;
+                i--;
+            }
+        }
+        return nums.length - count;
+    }
+    //way2:
+
 }
