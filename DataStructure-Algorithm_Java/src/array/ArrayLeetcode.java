14c238c5a2a13b609ac69d657f25dcac4aceceff
NickyViNi <happylemonlcm@gmail.com>
2024-04-23 14:26:57 -0700

    remove duplicates

--- DataStructure-Algorithm_Java/src/array/ArrayLeetcode.java
+++ DataStructure-Algorithm_Java/src/array/ArrayLeetcode.java
@@ -181,4 +181,14 @@ public class ArrayLeetcode {
         }
     }
 
+    //80 medium -> remove duplicates more than two from sorted array
+    public int removeDuplicates2(int[] nums) {
+        int k = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if ( k == 1 || nums[i] != nums[k - 2] ) {
+                nums[k++] = nums[i];
+            }
+        }
+        return k;
+    }
 }
