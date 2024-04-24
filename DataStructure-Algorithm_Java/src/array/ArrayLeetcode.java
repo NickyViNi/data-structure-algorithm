0721d7c88711675951f38a2c5cb5bd9ea6002ab6
NickyViNi <happylemonlcm@gmail.com>
2024-04-23 18:37:04 -0700

    122 way2

--- DataStructure-Algorithm_Java/src/array/ArrayLeetcode.java
+++ DataStructure-Algorithm_Java/src/array/ArrayLeetcode.java
@@ -269,5 +269,15 @@ public class ArrayLeetcode {
         }
         return profit;
     }
+    //122
+    public int maxProfitII2(int[] prices) {
+        int profit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i - 1]) {
+                profit += prices[i] - prices[i - 1];
+            }
+        }
+        return profit;
+    }
 
 }
