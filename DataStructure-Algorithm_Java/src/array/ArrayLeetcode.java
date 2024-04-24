e07f4662ea95efad8da6cba72cf399cf6248a7a0
NickyViNi <happylemonlcm@gmail.com>
2024-04-24 11:07:12 -0700

    find longest substirng

--- DataStructure-Algorithm_Java/src/array/ArrayLeetcode.java
+++ DataStructure-Algorithm_Java/src/array/ArrayLeetcode.java
@@ -1,7 +1,9 @@
 package array;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class ArrayLeetcode {
 
@@ -300,4 +302,16 @@ public class ArrayLeetcode {
         return minLen;
     }
 
+    //3 medium -> Given a string s, find the length of the longest substring without repeating characters.
+    public int lengthOfLongestSubstring(String s) {
+        Set<Character> set = new HashSet<>();
+        int maxLen = 0, left = 0;
+        for (int right = 0; right < s.length(); right++) {
+            while(!set.add(s.charAt(right))) {
+                set.remove(s.charAt(right));
+            }
+            maxLen = Math.max(maxLen, right - left + 1);
+        }
+        return maxLen;
+    }
 }
