ff45bc003ace19b191e89ae05ee342ca24f198ca
NickyViNi <happylemonlcm@gmail.com>
2024-04-24 00:24:31 -0700

    silding window : Minumum size subarray sum

--- DataStructure-Algorithm_Java/src/array/ArrayLeetcode.java
+++ DataStructure-Algorithm_Java/src/array/ArrayLeetcode.java
@@ -280,4 +280,24 @@ public class ArrayLeetcode {
         return profit;
     }
 
+    //209 medium -> Minumum size subarray sum
+    /*Given an array of positive integers nums and a positive integer target, return the minimal length of a
+    subarray whose sum is greater than or equal to target. If there is no such subarray, return 0 instead. */
+    public int minSubArrayLen(int target, int[] nums) {
+
+        int left = 0, right = 0, currSum = 0, minLen = Integer.MAX_VALUE;
+
+        while (right < nums.length) {
+            currSum += nums[right];
+            right++;
+            while (currSum >= target) {
+                currSum -= nums[left];
+                minLen = Math.min(minLen, right - left );
+                left++;
+            }
+        }
+        minLen = minLen == Integer.MAX_VALUE ? 0 : minLen;
+        return minLen;
+    }
+
 }
