f24738b61d60a2ed46a8e2122f06bf9a542b01f0
NickyViNi <happylemonlcm@gmail.com>
2024-04-23 17:24:34 -0700

    convert roman to integer

--- DataStructure-Algorithm_Java/src/array/ArrayLeetcode.java
+++ DataStructure-Algorithm_Java/src/array/ArrayLeetcode.java
@@ -235,4 +235,25 @@ public class ArrayLeetcode {
         return -1;
     }
 
+    //13 easy -> roman to integer
+    public int romanToInt(String s) {
+        Map<Character, Integer> hash = new HashMap<>();
+        hash.put('I', 1);
+        hash.put('V', 5);
+        hash.put('X', 10);
+        hash.put('L', 50);
+        hash.put('C', 100);
+        hash.put('D', 500);
+        hash.put('M', 1000);
+        int number = 0;
+        for (int i = 0; i < s.length() - 1; i++) {
+            if (hash.get(s.charAt(i)) >= hash.get(s.charAt(i + 1))) {
+                number +=  hash.get(s.charAt(i));
+            } else {
+                number -= hash.get(s.charAt(i));
+            }
+        }
+        return number + hash.get(s.charAt(s.length() - 1));
+    }
+
 }
