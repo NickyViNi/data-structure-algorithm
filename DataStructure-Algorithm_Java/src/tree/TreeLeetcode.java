6fe871beff2a71178676a95976aadf6bfce17275
NickyViNi <happylemonlcm@gmail.com>
2024-04-25 16:28:49 -0700

    symetric tree

--- DataStructure-Algorithm_Java/src/tree/TreeLeetcode.java
+++ DataStructure-Algorithm_Java/src/tree/TreeLeetcode.java
@@ -132,4 +132,15 @@ public class TreeLeetcode {
         return true;
     }
 
+    //101 easy -> Symmetric Tree
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) return true;
+        return isMirror(root.left, root.right);
+    }
+    private boolean isMirror(TreeNode a, TreeNode b) {
+        if (a == null && b == null) return true;
+        if ((a == null || b == null)) return false;
+        return a.val == b.val ? isMirror(a.left, b.right) && isMirror(a.right, b.left) : false;
+    }
+
 }
