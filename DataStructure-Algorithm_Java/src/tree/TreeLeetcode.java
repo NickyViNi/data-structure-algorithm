ebff3bb992e47f22262fde81c28fd32911d182b4
NickyViNi <happylemonlcm@gmail.com>
2024-04-25 15:44:09 -0700

    same tree - recursive

--- DataStructure-Algorithm_Java/src/tree/TreeLeetcode.java
+++ DataStructure-Algorithm_Java/src/tree/TreeLeetcode.java
@@ -104,6 +104,11 @@ public class TreeLeetcode {
     public boolean isSameTree(TreeNode p, TreeNode q) {
         if (p == null && q == null) return true;
         if ((p == null || q == null)) return false;
+        return p.val == q.val ? isSameTree(p.left, q.left) && isSameTree(p.right, q.right) : false;
+    }
+    public boolean isSameTree2(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
+        if ((p == null || q == null)) return false;
         TreeNode currp = p;
         TreeNode currq = q;
         Queue<TreeNode> queueP = new LinkedList<>();
