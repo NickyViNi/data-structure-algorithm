6eb955bb7260d4f869e9cf12fa6b1937d964e9ca
NickyViNi <happylemonlcm@gmail.com>
2024-04-26 16:43:09 -0700

    215 Kth largest ele in array

--- DataStructure-Algorithm_Java/src/array/ArrayLeetcode.java
+++ DataStructure-Algorithm_Java/src/array/ArrayLeetcode.java
@@ -6,6 +6,7 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.PriorityQueue;
 import java.util.Set;
 
 public class ArrayLeetcode {
@@ -512,7 +513,7 @@ public class ArrayLeetcode {
         return words[words.length - 1].length();
     }
 
-    //11 medium -> cotainer with most water
+    //11 medium -> cotainer with most water : two pointers
     public int maxArea(int[] height) {
         int left = 0, right = height.length - 1, maxArea = 0;
         while (left < right) {
@@ -523,4 +524,21 @@ public class ArrayLeetcode {
         }
         return maxArea;
     }
+
+    //215 medium -> Kth largest element in an array
+    public int findKthLargest(int[] nums, int k) {
+        if (nums.length == 1) return nums[0];
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a, b) -> b - a);
+        for (int num : nums) {
+            maxHeap.add(num);
+        }
+        int count = 0;
+        while (maxHeap.size() > 0) {
+            int knum = maxHeap.poll();
+            count++;
+            if (count == k) return knum;
+        }
+        return -1;
+    }
+
 }
