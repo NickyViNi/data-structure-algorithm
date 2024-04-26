f9a0b61a2492ddd6be3713bcde7f7a15b340d647
NickyViNi <happylemonlcm@gmail.com>
2024-04-26 00:18:14 -0700

    add two number

--- DataStructure-Algorithm_Java/src/linkedList/LLleetcode.java
+++ DataStructure-Algorithm_Java/src/linkedList/LLleetcode.java
@@ -324,4 +324,23 @@ public class LLleetcode {
         }
         return dummy.next;
     }
+    //2 medium -> add two number
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+        int carry = 0;
+        while (l1 != null || l2 != null || carry != 0) {
+            int num1 = l1 != null ? l1.val : 0;
+            int num2 = l2 != null ? l2.val : 0;
+            int sum = num1 + num2 + carry;
+            int remainder = sum % 10;
+            carry = sum / 10;
+            ListNode newNode = new ListNode(remainder);
+            curr.next = newNode;
+            curr = newNode;
+            l1 = l1 != null ? l1.next : null;
+            l2 = l2 != null ? l2.next : null;
+        }
+        return dummy.next;
+    }
 }
