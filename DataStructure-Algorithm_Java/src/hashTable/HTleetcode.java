716aad2d7606eac074021d8a6c7eb4a86d137a48
NickyViNi <happylemonlcm@gmail.com>
2024-04-30 10:28:11 -0700

    valid anagram way3

--- DataStructure-Algorithm_Java/src/hashTable/HTleetcode.java
+++ DataStructure-Algorithm_Java/src/hashTable/HTleetcode.java
@@ -378,4 +378,18 @@ public class HTleetcode {
         }
         return true;
     }
+    public boolean isAnagram3(String s, String t) {
+        if (s.length() != t.length()) return false;
+        HashMap<Character, Integer> hm = new HashMap<>();
+        for (char schar : s.toCharArray()) {
+            hm.put(schar, hm.getOrDefault(schar, 0) + 1);
+        }
+        for (char tchar : t.toCharArray()) {
+            hm.put(tchar, hm.getOrDefault(tchar, 0) - 1);
+        }
+        for (int val : hm.values()) {
+            if (val < 0) return false;
+        }
+        return true;
+    }
 }
