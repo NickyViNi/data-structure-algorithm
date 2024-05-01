70984ac3dfc09f7c5c33c1fa5ee64af26dc4477b
NickyViNi <happylemonlcm@gmail.com>
2024-04-30 17:38:12 -0700

    contians duplicate

--- DataStructure-Algorithm_Java/src/hashTable/HTleetcode.java
+++ DataStructure-Algorithm_Java/src/hashTable/HTleetcode.java
@@ -406,4 +406,17 @@ public class HTleetcode {
         }
         return result;
     }
+    //219 easy -> contains duplicate II
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (!hm.containsKey(nums[i])) {
+                hm.put(nums[i], i);
+            } else {
+                if (i - hm.get(nums[i]) <= k) return true;
+                hm.put(nums[i], i);
+            }
+        }
+        return false;
+    }
 }
