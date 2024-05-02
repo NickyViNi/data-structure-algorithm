a1993a3910e5700f2f1cc0726a6fee4e4979311e
NickyViNi <happylemonlcm@gmail.com>
2024-05-01 23:18:03 -0700

    path sum

--- DataStructure-Algorithm_Java/src/tree/TreeLeetcode.java
+++ DataStructure-Algorithm_Java/src/tree/TreeLeetcode.java
@@ -197,4 +197,13 @@ public class TreeLeetcode {
         }
         return count;
     }
+
+    //112 easy -> path sum
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if (root == null) return false;
+        if (root.left == null && root.right == null) {
+            return targetSum == root.val;
+        }
+        return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val);
+    }
 }
