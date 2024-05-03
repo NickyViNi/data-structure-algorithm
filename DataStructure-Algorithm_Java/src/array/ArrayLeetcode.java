76ac50be6d58fde3fec11133653e644180395c60
NickyViNi <happylemonlcm@gmail.com>
2024-05-03 11:26:19 -0700

    merge string alternately - use StringBuilder

--- DataStructure-Algorithm_Java/src/array/ArrayLeetcode.java
+++ DataStructure-Algorithm_Java/src/array/ArrayLeetcode.java
@@ -679,4 +679,13 @@ public class ArrayLeetcode {
         if (len2 < word2.length()) merge += word2.substring(len2, word2.length());
         return merge;
     }
+    public String mergeAlternately2(String word1, String word2) {
+        StringBuilder merge = new StringBuilder();
+        int maxLen = Math.max(word1.length(), word2.length());
+        for (int i = 0; i < maxLen; i++) {
+            if (i < word1.length()) merge.append(word1.charAt(i));
+            if (i < word2.length()) merge.append(word2.charAt(i));
+        }
+        return merge.toString();
+    }
 }
