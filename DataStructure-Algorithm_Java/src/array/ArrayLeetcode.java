fd252d3434c060ca2b774092315705ddb3a53386
NickyViNi <happylemonlcm@gmail.com>
2024-05-05 00:09:20 -0700

    flowerbed

--- DataStructure-Algorithm_Java/src/array/ArrayLeetcode.java
+++ DataStructure-Algorithm_Java/src/array/ArrayLeetcode.java
@@ -738,4 +738,17 @@ public class ArrayLeetcode {
         }
         return result;
     }
+    //605 easy -> can place flowers
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        if (n == 0) return true;
+        for (int i = 0; i < flowerbed.length; i++) {
+            if (flowerbed[i] == 0 && (i == 0 || flowerbed[i-1] == 0) &&
+            (i == flowerbed.length - 1 || flowerbed[i+1] == 0)) {
+                n--;
+                flowerbed[i] = 1;
+                if (n == 0) return true;
+            }
+        }
+        return false;
+    }
 }
