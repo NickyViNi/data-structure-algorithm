84b44c585f30281a3d46f4a99b201d031c8f1b20
NickyViNi <happylemonlcm@gmail.com>
2024-05-07 14:26:58 -0700

    minStack using one stack

---
+++ DataStructure-Algorithm_Java/src/stack/MinStack155.java
@@ -0,0 +1,33 @@
+package stack;
+import java.util.Stack;
+
+public class MinStack155 {
+    int min;
+    Stack<Integer> stack;
+    public MinStack155() {
+        this.stack = new Stack<>();
+        this.min = Integer.MAX_VALUE;
+    }
+
+    public void push(int val) {
+        if (val <= min) {
+            stack.push(min);
+            min = val;
+        }
+        stack.push(val);
+    }
+
+    public void pop() {
+        if (stack.pop() == min) {
+            min = stack.pop();
+        }
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return min;
+    }
+}
