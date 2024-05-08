cb343b318a46cbbcb58434073f62a27c8f125f17
NickyViNi <happylemonlcm@gmail.com>
2024-05-07 23:58:42 -0700

    product of an array

--- DataStructure-Algorithm_Java/src/array/ArrayLeetcode.java
+++ DataStructure-Algorithm_Java/src/array/ArrayLeetcode.java
@@ -780,4 +780,23 @@ public class ArrayLeetcode {
         }
         return end;
     }
+
+    //238 medium -> Product of array except self
+    public int[] productExceptSelf(int[] nums) {
+        int[] result = new int[nums.length];
+        result[0] = 1;
+        //traverse from left to right:
+        int curr = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            result[i] = curr;
+            curr *= nums[i];
+        }
+        //traverse from right to left:
+        curr = nums[nums.length - 1];
+        for (int j = nums.length - 2; j >= 0; j--) {
+            result[j] *= curr;
+            curr *= nums[j];
+        }
+        return result;
+    }
 }
