57afc3139cd63244d85403bf658dbafcebeb77bb
NickyViNi <happylemonlcm@gmail.com>
2024-05-09 16:36:42 -0700

    perfect binary tree adding next pointer: recursive

--- DataStructure-Algorithm_Java/src/tree/TreeLeetcode.java
+++ DataStructure-Algorithm_Java/src/tree/TreeLeetcode.java
@@ -296,4 +296,12 @@ public class TreeLeetcode {
         }
         return root;
     }
+    public TreeNode connect2 (TreeNode root) {
+        if (root == null) return root;
+        if (root.left != null) root.left.next = root.right;
+        if (root.right != null && root.next != null) root.right.next = root.next.left;
+        connect(root.left);
+        connect(root.right);
+        return root;
+    }
 }
