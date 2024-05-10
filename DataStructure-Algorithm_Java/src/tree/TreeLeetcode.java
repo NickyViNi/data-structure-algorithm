b0c3aff5939de6cd3ac413c6b81fff70479c333f
NickyViNi <happylemonlcm@gmail.com>
2024-05-10 14:50:11 -0700

    LCA of binary tree

--- DataStructure-Algorithm_Java/src/tree/TreeLeetcode.java
+++ DataStructure-Algorithm_Java/src/tree/TreeLeetcode.java
@@ -382,4 +382,13 @@ public class TreeLeetcode {
             return true;
         }
     }
+
+    //236 medium -> Lowest Common Ancestor of Binary Tree
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q) return root;
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left != null && right != null) return root;
+        return left == null ? right : left;
+    }
 }
