15f489c2491346b7992205072305fc5779ecc726
NickyViNi <happylemonlcm@gmail.com>
2024-05-10 15:36:04 -0700

    binary tree lever order traversal

--- DataStructure-Algorithm_Java/src/tree/TreeLeetcode.java
+++ DataStructure-Algorithm_Java/src/tree/TreeLeetcode.java
@@ -398,7 +398,7 @@ public class TreeLeetcode {
         Queue<TreeNode> queue = new LinkedList<>();
         if (root == null) return result;
         queue.offer(root);
-        while (queue.size() > 0) {
+        while (queue.size() > 0) { // !queue.isEmpty()
             int size = queue.size();
             for (int i = 0; i < size; i++) {
                 TreeNode node = queue.poll();
@@ -406,6 +406,26 @@ public class TreeLeetcode {
                 if (node.right != null) queue.offer(node.right);
                 if (node.left != null) queue.offer(node.left);
             }
+        }
+        return result;
+    }
+
+    //102 medium -> Binary Tree level order travelal
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) return result;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> list = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                list.add(node.val);
+                if (node.left != null) queue.offer(node.left);
+                if (node.right != null) queue.offer(node.right);
+            }
+            result.add(list);
         }
         return result;
     }
