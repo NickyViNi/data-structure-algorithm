1c977a8b4543b7d2b6104463d040dca2e0d4a156
NickyViNi <happylemonlcm@gmail.com>
2024-05-14 01:26:20 -0700

    find peek element way2

--- DataStructure-Algorithm_Java/src/array/ArrayLeetcode.java
+++ DataStructure-Algorithm_Java/src/array/ArrayLeetcode.java
@@ -1044,4 +1044,20 @@ public class ArrayLeetcode {
         }
         return -1;
     }
+
+    public int findPeakElement2(int[] nums) {
+        if (nums.length == 1) return 0;
+        int start = 0;
+        int end = nums.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            int prev = mid - 1 >= 0 ? nums[mid-1] : Integer.MIN_VALUE;
+            int after = mid + 1 < nums.length ? nums[mid+1] : Integer.MIN_VALUE;
+            int curr = nums[mid];
+            if (prev < curr && curr > after) return mid;
+            else if (prev > curr) end = mid - 1;
+            else if (after > curr) start = mid + 1;
+        }
+        return -1;
+    }
 }
