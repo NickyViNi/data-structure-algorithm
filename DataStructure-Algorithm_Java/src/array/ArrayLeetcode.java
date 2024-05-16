6a367040895b21caefb77a3069e9ea470a60337c
NickyViNi <happylemonlcm@gmail.com>
2024-05-15 17:11:59 -0700

    find minimum in rotated sorted array

--- DataStructure-Algorithm_Java/src/array/ArrayLeetcode.java
+++ DataStructure-Algorithm_Java/src/array/ArrayLeetcode.java
@@ -1168,4 +1168,21 @@ public class ArrayLeetcode {
         }
         return idx;
     }
+
+    //153 medium -> binary search:find minimum in rotated sorted array
+    public int findMin(int[] nums) {
+        int left = 0, right = nums.length - 1;
+        if (nums[left] <= nums[right]) return nums[left];
+        while (left + 1 < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] > nums[left]) {
+                left = mid;
+            } else {
+                right = mid;
+            }
+        }
+        return nums[left] <= nums[right] ? nums[left] : nums[right];
+    }
+
+
 }
