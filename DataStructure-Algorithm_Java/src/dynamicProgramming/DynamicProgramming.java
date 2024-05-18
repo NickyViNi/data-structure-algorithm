0abc710f98eed40199f8f1e419730370b71f4721
NickyViNi <happylemonlcm@gmail.com>
2024-05-17 17:19:38 -0700

    house robber

--- DataStructure-Algorithm_Java/src/dynamicProgramming/DynamicProgramming.java
+++ DataStructure-Algorithm_Java/src/dynamicProgramming/DynamicProgramming.java
@@ -14,4 +14,18 @@ public class DynamicProgramming {
         }
         return curr;
     }
+
+    //198 medium -> house robber
+    public int rob(int[] nums) {
+        if (nums.length == 1) return nums[0];
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+        if (nums.length == 2) return dp[1];
+        for (int i = 2; i < nums.length; i++) {
+            dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
+        }
+        return dp[dp.length - 1];
+    }
+    // sums: [2,7,9,3,1], dp: [2,7,11,11,12]
 }
