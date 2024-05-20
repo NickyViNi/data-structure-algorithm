a3aa10f77a8e12202952794cb9975f9820b4c403
NickyViNi <happylemonlcm@gmail.com>
2024-05-20 15:15:44 -0700

    coin change

--- DataStructure-Algorithm_Java/src/dynamicProgramming/DynamicProgramming.java
+++ DataStructure-Algorithm_Java/src/dynamicProgramming/DynamicProgramming.java
@@ -59,7 +59,7 @@ public class DynamicProgramming {
         return dp[s.length()];
     }
 
-    //120 medium -> trangle
+    //120 medium -> triangle
     //way 1:
     public int minimumTotal1(List<List<Integer>> triangle) {
         for (int i = triangle.size() - 2; i >= 0 ; i--) {
@@ -122,4 +122,18 @@ public class DynamicProgramming {
         return minPath;
     }
 
+    //322 medium -> coin change
+    public int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+        for (int i = 1; i <= amount; i++) { //i means amount, keep minimum number of coins to make up each amout
+            for (int coin : coins) {
+                if (i - coin >= 0 && dp[i - coin] != Integer.MAX_VALUE) {
+                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+                }
+            }
+        }
+        return dp[amount] == Integer.MAX_VALUE ? -1 : dp[amount];
+    }
 }
