39de2d9c9a1c66403edad602dea686173648d613
NickyViNi <happylemonlcm@gmail.com>
2024-05-19 23:22:47 -0700

    merge sort in LinkedList

--- DataStructure-Algorithm_Java/src/linkedList/LLleetcode.java
+++ DataStructure-Algorithm_Java/src/linkedList/LLleetcode.java
@@ -441,4 +441,37 @@ public class LLleetcode {
         }
         return len;
     }
+
+    //148 medium -> merge sort in LinkedList
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode midNode = getMiddleNode(head);
+        ListNode nextNode = midNode.next;
+        midNode.next = null;
+        return mergeList(sortList(head), sortList(nextNode));
+    }
+    private ListNode getMiddleNode(ListNode node) {
+        ListNode slow = node, fast = node;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+    private ListNode mergeList(ListNode a, ListNode b) {
+        ListNode dummy = new ListNode(-1);
+        ListNode curr = dummy;
+        while (a != null && b != null) {
+            if (a.val <= b.val) {
+                curr.next = a;
+                a = a.next;
+            } else {
+                curr.next = b;
+                b = b.next;
+            }
+            curr = curr.next;
+        }
+        curr.next = a == null ? b : a;
+        return dummy.next;
+    }
 }
