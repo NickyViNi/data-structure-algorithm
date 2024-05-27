bda6ab701d34521ed468bce1145f9451abf5ea02
NickyViNi <happylemonlcm@gmail.com>
2024-05-26 21:39:37 -0700

    find pivot index

--- DataStructure-Algorithm_Java/src/array/ArrayLeetcode.java
+++ DataStructure-Algorithm_Java/src/array/ArrayLeetcode.java
@@ -1408,4 +1408,15 @@ public class ArrayLeetcode {
         }
         return max;
     }
+
+    //724 easy -> find pivot index
+    public int pivotIndex(int[] nums) {
+        int sum = 0, leftSum = 0;
+        for (int num : nums) sum += num;
+        for (int i = 0; i < nums.length; i++) {
+            if (leftSum == sum - leftSum - nums[i]) return i;
+            leftSum += nums[i];
+        }
+        return -1;
+    }
 }
