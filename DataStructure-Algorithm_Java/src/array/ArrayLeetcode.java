55c1de4a310b61a0b16cbd5e130a117a74977dfc
NickyViNi <happylemonlcm@gmail.com>
2024-05-27 13:21:56 -0700

    move zeroes

--- DataStructure-Algorithm_Java/src/array/ArrayLeetcode.java
+++ DataStructure-Algorithm_Java/src/array/ArrayLeetcode.java
@@ -1475,4 +1475,26 @@ public class ArrayLeetcode {
         }
         return max;
     }
+
+    //283 easy -> move zeroes
+    public void moveZeroes2(int[] nums) {
+        int zeroIdx = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                int temp = nums[i];
+                nums[i] = nums[zeroIdx];
+                nums[zeroIdx++] = temp;
+            }
+        }
+    }
+
+    public void moveZeroes(int[] nums) {
+        int k = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) nums[k++] = nums[i];
+        }
+        while (k < nums.length) {
+            nums[k++] = 0;
+        }
+    }
 }
