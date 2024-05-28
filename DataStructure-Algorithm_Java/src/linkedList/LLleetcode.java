d82aa33cfad6ecea24edcb371ffd3f43a0253b96
NickyViNi <happylemonlcm@gmail.com>
2024-05-28 16:27:18 -0700

    reverse linkedList iterative approach

--- DataStructure-Algorithm_Java/src/linkedList/LLleetcode.java
+++ DataStructure-Algorithm_Java/src/linkedList/LLleetcode.java
@@ -474,4 +474,18 @@ public class LLleetcode {
         curr.next = a == null ? b : a;
         return dummy.next;
     }
+
+    //206 easy -> Reverse Linked List
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode nextNode = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextNode;
+        }
+        return prev;
+    }
 }
