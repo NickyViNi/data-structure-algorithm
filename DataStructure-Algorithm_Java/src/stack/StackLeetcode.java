d0a056583d0f8edbf357445dc4e2d0e03283f6d4
NickyViNi <happylemonlcm@gmail.com>
2024-05-28 00:46:34 -0700

    Removing Stars From a String

--- DataStructure-Algorithm_Java/src/stack/StackLeetcode.java
+++ DataStructure-Algorithm_Java/src/stack/StackLeetcode.java
@@ -43,4 +43,39 @@ public class StackLeetcode {
         }
         return stack.pop();
     }
+
+    //2390 medium -> Removing Stars From a String
+    public String removeStars(String s) {
+        Stack<Character> stack = new Stack<>();
+        StringBuilder str = new StringBuilder();
+        for (char ch : s.toCharArray()) {
+            if (ch == '*') {
+                stack.pop();
+            } else {
+                stack.push(ch);
+            }
+        }
+        while (!stack.empty()) str.append(stack.pop());
+        return str.reverse().toString();
+    }
+
+    // way2 is too slow
+    public String removeStars2(String s) {
+        Stack<Character> stack = new Stack<>();
+        String res = "";
+        for (char ch : s.toCharArray()) stack.push(ch);
+        int stars = 0;
+        while (!stack.empty()) {
+            while (stack.peek() == '*') {
+                stack.pop();
+                stars++;
+            }
+            while (stars > 0 && stack.peek() != '*') {
+                stack.pop();
+                stars--;
+            }
+            if (!stack.empty() && stack.peek() != '*') res = stack.pop() + res;
+        }
+        return res;
+    }
 }
