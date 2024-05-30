bb7117b09cf46360115d5b502d41055f6f180812
NickyViNi <happylemonlcm@gmail.com>
2024-05-29 18:57:45 -0700

    search in a BST

--- DataStructure-Algorithm_Java/src/tree/TreeLeetcode.java
+++ DataStructure-Algorithm_Java/src/tree/TreeLeetcode.java
@@ -483,4 +483,26 @@ public class TreeLeetcode {
         getLeaf(root.left, list);
         getLeaf(root.right, list);
     }
+    //700 easy -> Search in a Binary Search Tree
+    //recursion
+    public TreeNode searchBST1(TreeNode root, int val) {
+        if (root == null) return null;
+        if (root.val == val) return root;
+        if (root.val > val) return searchBST(root.left, val);
+        else return searchBST(root.right, val);
+    }
+    //iterative
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null) return null;
+        while (root != null) {
+            if (root.val > val) {
+                root = root.left;
+            } else if (root.val < val) {
+                root = root.right;
+            } else {
+                return root;
+            }
+        }
+        return root;
+    }
 }
