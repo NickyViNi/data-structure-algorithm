a1051757cc2307a39504e5dd47e28acd2fd21f64
NickyViNi <happylemonlcm@gmail.com>
2024-06-14 11:05:21 -0700

    daily temperatures

--- DataStructure-Algorithm_Java/src/stack/StackLeetcode.java
+++ DataStructure-Algorithm_Java/src/stack/StackLeetcode.java
@@ -135,4 +135,18 @@ public class StackLeetcode {
         while (!dict.empty()) res.append(dict.pop());
         return res.reverse().toString();
     }
+
+    //739 medium -> daily temparetures
+    public int[] dailyTemperatures(int[] temperatures) {
+        int[] result = new int[temperatures.length];
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < temperatures.length; i++) {
+            while (!stack.empty() && temperatures[i] > temperatures[stack.peek()]) {
+                int idx = stack.pop();
+                result[idx] = i - idx;
+            }
+            stack.push(i);
+        }
+        return result;
+    }
 }
