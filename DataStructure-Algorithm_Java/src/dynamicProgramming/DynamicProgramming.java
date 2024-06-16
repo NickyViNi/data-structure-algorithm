5fdb8c9106a110f11d11b07ca5d70392d457d11b
NickyViNi <happylemonlcm@gmail.com>
2024-06-16 01:28:53 -0700

    min cost climbing stairs

--- DataStructure-Algorithm_Java/src/dynamicProgramming/DynamicProgramming.java
+++ DataStructure-Algorithm_Java/src/dynamicProgramming/DynamicProgramming.java
@@ -181,4 +181,12 @@ public class DynamicProgramming {
         }
         return Arrays.stream(dp).max().orElse(0);
     }
+
+    //746 easy -> Min Cost Climbing Stairs
+    public int minCostClimbingStairs(int[] cost) {
+        for (int i = cost.length - 3; i >= 0; i--) {
+            cost[i] += Math.min(cost[i + 1], cost[i + 2]);
+        }
+        return Math.min(cost[0], cost[1]);
+    }
 }
