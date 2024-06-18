e676f58b133e1b3fdfee825258fa51c258a34a5f
NickyViNi <happylemonlcm@gmail.com>
2024-06-17 23:54:28 -0700

    flips a | b == c

--- DataStructure-Algorithm_Java/src/array/Binary.java
+++ DataStructure-Algorithm_Java/src/array/Binary.java
@@ -89,4 +89,21 @@ public class Binary {
         }
         return left << count;
     }
+
+    //1318 medium -> Minimum Flips to Make a OR b Equal to c
+    public int minFlips(int a, int b, int c) {
+        int flips = 0;
+        for (int i = 0; i < 32; i++) {
+            int aBit = (a >> i) & 1;
+            int bBit = (b >> i) & 1;
+            int cBit = (c >> i) & 1;
+            if (cBit == 0) {
+                if (aBit == 1) flips++;
+                if (bBit == 1) flips++;
+            } else {
+                if (aBit == 0 && bBit == 0) flips++;
+            }
+        }
+        return flips;
+    }
 }
