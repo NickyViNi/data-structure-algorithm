064e00730e88fc1faac38fc4889f2e23fce70630
NickyViNi <happylemonlcm@gmail.com>
2024-06-20 11:50:24 -0700

    dp - edit distance

--- DataStructure-Algorithm_Java/src/dynamicProgramming/DynamicProgramming.java
+++ DataStructure-Algorithm_Java/src/dynamicProgramming/DynamicProgramming.java
@@ -314,9 +314,33 @@ public class DynamicProgramming {
     public int maxProfit(int[] prices, int fee) {
         int bought = -prices[0], profit = 0;
         for (int i = 1; i < prices.length; i++) {
+            //subtract prices[i] from current profit when buying a stock:
             bought = Math.max(bought, profit - prices[i]);
+            //add prices[i] and subtract fee from current profit when selling a stock:
             profit = Math.max(profit, prices[i] + bought - fee);
         }
         return profit;
+    }
+
+    //72 medium -> edit distance
+    public int minDistance(String word1, String word2) {
+        int len1 = word1.length(), len2 = word2.length();
+        int[][] dp = new int[len1 + 1][len2 + 1];
+        for (int i = 0; i <= len1; i++) {
+            dp[i][0] = i;
+        }
+        for (int j = 0; j <= len2; j++) {
+            dp[0][j] = j;
+        }
+        for (int i = 1; i <= len1; i++) {
+            for (int j = 1; j <= len2; j++) {
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = 1 + Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1]));
+                }
+            }
+        }
+        return dp[len1][len2];
     }
 }
