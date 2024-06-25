81654864bfd442912410295ee45ac252af939808
NickyViNi <happylemonlcm@gmail.com>
2024-06-25 16:49:45 -0700

    reorder city to zero

--- DataStructure-Algorithm_Java/src/graph/MatrixLeetcode.java
+++ DataStructure-Algorithm_Java/src/graph/MatrixLeetcode.java
@@ -562,4 +562,29 @@ public class MatrixLeetcode {
 
         return -1;
     }
+
+    //1466 medium -> Reorder Routes to Make All Paths Lead to the City Zero
+    //dfs
+    public int minReorder1(int n, int[][] connections) {
+        List<List<Integer>> list = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            list.add(new ArrayList<>());
+        }
+        for (int[] c : connections) {
+            list.get(c[0]).add(c[1]);
+            list.get(c[1]).add(-c[0]);
+        }
+        return reorderDfs(list, new boolean[n], 0);
+    }
+    private int reorderDfs(List<List<Integer>> list, boolean[] visited, int start) {
+        int reorder = 0;
+        visited[start] = true;
+        for (int li : list.get(start)) {
+            if (!visited[Math.abs(li)]) {
+                reorder += reorderDfs(list, visited, Math.abs(li)) + (li > 0 ? 1 : 0);
+            }
+        }
+        return reorder;
+    }
+
 }
